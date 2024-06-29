5e3a4699c2022ec8e021c13338bd4ad4490d5d57
LamNguyen <lnguyen4e@gmail.com>
2024-06-29 15:04:27 -0400

    Redirect with month end point

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,38 @@
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render
 
-# Create your views here.
+monthly_challenges = {
+    "january": "Eat no meat for the entire month!",
+    "february": "Walk 5k everyday",
+    "march": "Learn Django for at least 20 minutes a day",
+    "april": "Eat no meat for the entire month!",
+    "may": "Walk 5k everyday",
+    "june": "Learn Django for at least 20 minutes a day",
+    "july": "Eat no meat for the entire month!",
+    "august": "Walk 5k everyday",
+    "september": "Learn Django for at least 20 minutes a day",
+    "october": "Eat no meat for the entire month!",
+    "november": "Walk 5k everyday",
+    "december": "Learn Django for at least 20 minutes a day",
+}
+
+
+def monthly_challenge_by_number(request, month):
+    months = list(monthly_challenges.keys())
+
+    if (month > len(months)):
+        return HttpResponseNotFound("Invalid month")
+
+    redirect_month = months[month-1]
+
+    return HttpResponseRedirect("/challenges/" + redirect_month)
+
+
+def monthly_challenge(request, month):
+
+    try:
+        challenge_text = monthly_challenges[month]
+    except:
+        return HttpResponseNotFound("This month is not supported")
+
+    return HttpResponse(challenge_text)
